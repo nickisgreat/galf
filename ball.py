bc9ae88d831592a72ad871b00bf6476fef4e8044
nickisgreat <123120304+nickisgreat@users.noreply.github.com>
2023-01-30 16:01:48 -0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -5,35 +5,24 @@ class Ball(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.vel = [0, 0]
-        self.image = pygame.Surface([15, 15])
+        self.Lvel = [0, 0]
+        self.velop = 0
+        self.Lvelop = 0
+        self.image = pygame.Surface((15, 15))
         self.rect = self.image.get_rect()
         self.shoot = False
-        pygame.draw.rect(self.image, (255, 255, 255), [0, 0, 15, 15])
+        self.shootT = [False, False]
+        pygame.draw.circle(self.image, (255, 255, 255), self.rect.center, 7.5, 15)
     def update(self):
         if self.shoot:
-            if self.vel[0] < 0:
-                self.vel[0] += 0.1 * abs(self.vel[0] / self.vel[1])
-                if self.vel[0] > 0:
-                    self.vel[0] = 0
-                    self.shoot = False
-                    return
-            elif self.vel[0] > 0:
-                self.vel[0] -= 0.1 * abs(self.vel[0] / self.vel[1])
-                if self.vel[0] < 0:
-                    self.vel[0] = 0
-                    self.shoot = False
-                    return
-            if self.vel[1] > 0:
-                self.vel[1] -= 0.1 * abs(self.vel[1] / self.vel[0])
-                if self.vel[1] < 0:
-                    self.vel[1] = 0
-                    self.shoot = False
-                    return
-            elif self.vel[1] < 0:
-                self.vel[1] += 0.1 * abs(self.vel[1] / self.vel[0])
-                if self.vel[1] > 0:
-                    self.vel[1] = 0
-                    self.shoot = False
-                    return
+            self.vel = [(self.velop/self.Lvelop)*self.Lvel[0], (self.velop/self.Lvelop)*self.Lvel[1]]
+
             self.rect.x += self.vel[0]
             self.rect.y += self.vel[1]
+
+            if self.velop > 0:
+                self.velop -= 5
+            else:
+                self.shoot = False
+                self.velop = 0
+                self.vel = [0, 0]
